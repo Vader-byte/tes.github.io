9eb932a3699f841afbc7b2e3d6265d9e8eb95efb
Vader-byte <rizal.n614@gmail.com>
2023-02-19 07:56:52 +0700

    fix animasi button dan swal

--- main.js
+++ main.js
@@ -30,44 +30,24 @@ animateHeart()
 
 const t = document.querySelector('#disini');
 t.addEventListener('click', function() {
-    const swalWithBootstrapButtons = Swal.mixin({
-    customClass: {
-      confirmButton: 'btn btn-success',
-      cancelButton: 'btn btn-danger'
-    },
-    buttonsStyling: false
-  })
-  
-  swalWithBootstrapButtons.fire({
-    title: 'Are you sure?',
-    text: "You won't be able to revert this!",
-    icon: 'warning',
+  Swal.fire({
+    title: "test",
+    text: 'test text',
+    icon: 'question',
     showCancelButton: true,
-    confirmButtonText: 'Yes, delete it!',
-    cancelButtonText: 'No, cancel!',
-    reverseButtons: true
-    }).then((result) => {
-      if (result.isConfirmed) {
-        swalWithBootstrapButtons.fire(
-          'Deleted!',
-          'Your file has been deleted.',
+    confirmButtonText: 'klik',
+  }).then((res) => {
+      if(res.isConfirmed) {
+        Swal.fire(
+          'confirm',
           'success'
         )
-    } else if (
-      /* Read more about handling dismissals below */
-      result.dismiss === Swal.DismissReason.cancel
-    ) {
-      swalWithBootstrapButtons.fire(
-        'Cancelled',
-        'Your imaginary file is safe :)',
-        'error'
-      )
-    }
-  })
+      }
+    })
 })
 
 
-
+AOS.init()
 
 
 
